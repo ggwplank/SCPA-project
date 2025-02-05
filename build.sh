a22031d835223ef26f8eea94c240ff89feb03b62
simoneniro17 <simoneniro17@gmail.com>
2025-02-05 19:31:38 +0100

    aggiunto script per incremento QoL

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo ">>> Opening openmp..."
+cd openmp || exit 1  # Se fallisce, esce con errore
+
+echo ">>> Cleaning..."
+make clean
+
+echo ">>> Building..."
+if [[ "$1" == "run" ]]; then
+    make run MAT="$2"
+elif [[ "$1" == "run_openmp" ]]; then
+    make run_openmp MAT="$2"
+else
+    echo "Usage: $0 [run|run_openmp] nome.mtx"
+    exit 1
+fi
+
+cd ..
+
+# Usage: (DALLA ROOT DEL PROGETTO, NON DALLA CARTELLA openmp)
+# ./build.sh run matrice.mtx
+# ./build.sh run_openmp matrice.mtx
--- openmp/lib/vector_generator.h
+++ openmp/lib/vector_generator.h
@@ -1,1 +1,6 @@
+#ifndef VECTOR_GENERATOR_H
+#define VECTOR_GENERATOR_H
+
 void generate_random_vector(const char *matrix_name, int size, double **vector);
+
+#endif
