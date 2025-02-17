0248df8117f528859bb55d255d7d1f59ac3d8f90
ggwplank <brunori.lorenzo2001@gmail.com>
2025-02-17 09:52:57 +0100

    nuovi grafici

--- results/graph_builder_CSR_compare.py
+++ results/graph_builder_CSR_compare.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-def compare_matrices_non_compliant(file_path1, file_path2, output_path):
+def compare_matrices(file_path1, file_path2, output_path):
     # Carica il primo file CSV
     df1 = pd.read_csv(file_path1)
 
@@ -10,11 +10,11 @@ def compare_matrices_non_compliant(file_path1, file_path2, output_path):
     df1['avg_nnz_per_row'] = df1['NZ'] / df1['M']
     df1['density'] = df1['NZ'] / (df1['M'] * df1['N'])
 
-    # Filtra le matrici che non rispettano la condizione
-    df1_non_compliant = df1[(df1['avg_nnz_per_row'] >= 25) & (df1['density'] <= 1.5e-3)].copy()
+    # Filtra le matrici secondo la condizione specificata e fai una copia esplicita
+    df1_filtered = df1[(df1['avg_nnz_per_row'] < 25) | (df1['density'] > 1.5e-3)].copy()
 
     # Calcola la grandezza della matrice
-    df1_non_compliant.loc[:, 'Size'] = df1_non_compliant['NZ'] / (df1_non_compliant['M'] * df1_non_compliant['N'])
+    df1_filtered.loc[:, 'Size'] = df1_filtered['NZ'] / (df1_filtered['M'] * df1_filtered['N'])
 
     # Carica il secondo file CSV
     df2 = pd.read_csv(file_path2)
@@ -23,46 +23,45 @@ def compare_matrices_non_compliant(file_path1, file_path2, output_path):
     df2['avg_nnz_per_row'] = df2['NZ'] / df2['M']
     df2['density'] = df2['NZ'] / (df2['M'] * df2['N'])
 
-    # Filtra le matrici che non rispettano la condizione
-    df2_non_compliant = df2[(df2['avg_nnz_per_row'] >= 25) & (df2['density'] <= 1.5e-3)].copy()
+    # Filtra le matrici secondo la condizione specificata e fai una copia esplicita
+    df2_filtered = df2[(df2['avg_nnz_per_row'] < 25) | (df2['density'] > 1.5e-3)].copy()
 
     # Calcola la grandezza della matrice
-    df2_non_compliant.loc[:, 'Size'] = df2_non_compliant['NZ'] / (df2_non_compliant['M'] * df2_non_compliant['N'])
+    df2_filtered.loc[:, 'Size'] = df2_filtered['NZ'] / (df2_filtered['M'] * df2_filtered['N'])
 
     # Ordina i dati per grandezza della matrice
-    df1_non_compliant_sorted = df1_non_compliant.sort_values(by='Size')
-    df2_non_compliant_sorted = df2_non_compliant.sort_values(by='Size')
+    df1_sorted = df1_filtered.sort_values(by='Size')
+    df2_sorted = df2_filtered.sort_values(by='Size')
 
     # Seleziona solo le matrici comuni tra i due file
-    common_matrices = set(df1_non_compliant_sorted['Matrix']).intersection(set(df2_non_compliant_sorted['Matrix']))
+    common_matrices = set(df1_sorted['Matrix']).intersection(set(df2_sorted['Matrix']))
 
     # Filtra entrambi i DataFrame per contenere solo le matrici comuni
-    df1_non_compliant_common = df1_non_compliant_sorted[df1_non_compliant_sorted['Matrix'].isin(common_matrices)]
-    df2_non_compliant_common = df2_non_compliant_sorted[df2_non_compliant_sorted['Matrix'].isin(common_matrices)]
+    df1_common = df1_sorted[df1_sorted['Matrix'].isin(common_matrices)]
+    df2_common = df2_sorted[df2_sorted['Matrix'].isin(common_matrices)]
 
     # Ordina per grandezza della matrice
-    df1_non_compliant_common = df1_non_compliant_common.sort_values(by='Size')
-    df2_non_compliant_common = df2_non_compliant_common.sort_values(by='Size')
+    df1_common = df1_common.sort_values(by='Size')
+    df2_common = df2_common.sort_values(by='Size')
 
     # Traccia il grafico a barre per confrontare le matrici
     plt.figure(figsize=(12, 6))
 
     # Indici per posizionare le barre
-    x = range(len(df1_non_compliant_common))
+    x = range(len(df1_common))
 
     # Crea le barre per il primo file
-    plt.bar([i - 0.2 for i in x], df1_non_compliant_common['MedianGFlops'], width=0.4, label='Warp', color='skyblue')
+    plt.bar([i - 0.2 for i in x], df1_common['MedianGFlops'], width=0.4, label='Row based', color='skyblue')
 
     # Crea le barre per il secondo file
-    plt.bar([i + 0.2 for i in x], df2_non_compliant_common['MedianGFlops'], width=0.4, label='Row based', color='lightgreen')
+    plt.bar([i + 0.2 for i in x], df2_common['MedianGFlops'], width=0.4, label='Warp', color='lightgreen')
 
     # Etichetta gli assi
     plt.xlabel("Matrice")
     plt.ylabel("Median GFlops")
-    plt.title("Confronto delle matrici non compliant per Median GFlops tra due file")
 
     # Rotazione delle etichette sull'asse X per una migliore leggibilità
-    plt.xticks(x, df1_non_compliant_common['Matrix'], rotation=45, ha='right')
+    plt.xticks(x, df1_common['Matrix'].str.replace('.mtx', '', regex=False), rotation=45, ha='right')
 
     # Aggiungi la griglia per migliorare la leggibilità
     plt.grid(True, axis='y', linestyle='--', alpha=0.7)
@@ -83,5 +82,5 @@ def compare_matrices_non_compliant(file_path1, file_path2, output_path):
 # Esempio di utilizzo
 file_path1 = "performance-CSR-warp.csv"
 file_path2 = "performance-CSR.csv"
-output_path = "graphs/bigMAT-CSR-comaprison.png"
-compare_matrices_non_compliant(file_path1, file_path2, output_path)
+output_path = "graphs/littleMAT-CSR-comaprison.png"
+compare_matrices(file_path1, file_path2, output_path)
